2f9a8b35d47353aa1022ea4de438a1b3b260f97e
SaliheddineAbdellah <abdellah.saliheddine.101@gmail.com>
2023-06-14 23:01:51 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,6 +9,6 @@ int main(void)
 	printf("Size of an int: %li byte(s)\n", sizeof(int));
 	printf("Size of a long int: %li byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %li byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %li byte(s)\n", sizeof(float));
+	printf("Size of a float: %li byte(s)", sizeof(float));
 	return (0);
 }
