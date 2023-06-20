06aa91bfae8e13b9299dffd4618ec8c632a9a94d
SaliheddineAbdellah <abdellah.saliheddine.101@gmail.com>
2023-06-20 23:46:57 +0100

    functions nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet in lower case.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+	for(i = 0; i < 10; i++)
+	{
+		for(j = 97; j <= 122; j++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _islower - Shows 1 if the input is a 
+ * lower character. Another cases, shows
+ * 0
+ *
+ * @c: The character in ASCII code
+ *
+ * Return: 1 for lowercase character. 0 for the rest.
+ */
+int _islower(int c)
+{
+	if(c >='a' && c <='z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
