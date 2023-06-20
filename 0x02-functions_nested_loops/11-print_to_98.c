58db00a83759cb848ad85d422c2f74ac44de7662
SaliheddineAbdellah <abdellah.saliheddine.101@gmail.com>
2023-06-21 00:29:36 +0100

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,24 +8,25 @@
 void print_to_98(int n)
 {
 	int i, j;
-	if (n <= 98)
+
+	if (n <= 98 )
 	{
 		for (i = 0; i < 99; i++)
 		{
 			if (i != 98)
 				printf("%d, ", i);
 			else if (i == 98)
-				printf("%d, ", i);
+				printf("%d\n", i);
 		}
 	}
 	else if (n >= 98)
 	{
-		for (j = n;j >= 98; j++)
+		for (j = n; j >= 98; j++)
 		{
 			if (j != 98)
 				printf("%d, ", j);
 			else if (j == 98)
-				printf("%d, ", j);
+				printf("%d\n", j);
 		}
 	}
 }
