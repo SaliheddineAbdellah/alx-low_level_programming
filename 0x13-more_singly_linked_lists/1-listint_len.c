05ec3e0400d40ab4b3bb9fa8c457268e9b5ea16a
SaliheddineAbdellah <abdellah.saliheddine.101@gmail.com>
2023-07-31 14:09:44 -0700

    MORE SINGLY LINKED LISTS

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,7 +2,7 @@
 /**
  * listint_len - the number of elements in a linked list
  * @h: linked list to count
- * @Return: count of the list
+ * Return: count of the list
  */
 size_t listint_len(const listint_t *h)
 {
