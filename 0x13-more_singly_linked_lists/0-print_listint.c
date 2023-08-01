75872f0d245c31c339e0af7899889e0d6d96b034
SaliheddineAbdellah <abdellah.saliheddine.101@gmail.com>
2023-08-01 14:06:41 -0700

    SINGLY LINKED LISTS

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,19 +5,16 @@
  * @h: linked list to be printed
  * Return: list of elements
  */
-
 size_t print_listint(const listint_t *h)
 {
 	size_t count;
 
 	count = 0;
-
 	while (h)
 	{
 		printf("%d\n", h->n);
 		count++;
 		h = h->next;
 	}
-
 	return (count);
 }
