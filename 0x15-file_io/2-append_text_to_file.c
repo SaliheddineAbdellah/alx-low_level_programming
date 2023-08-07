c00418400723ee0c9f1bd07f6e59c6bd2533d2d9
SaliheddineAbdellah <abdellah.saliheddine.101@gmail.com>
2023-08-07 10:23:36 -0700

    FILES I O

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,19 +8,24 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, w, lenght = 0;
+	int o, w, len = 0;
 
-	if ( filename == NULL)
+	if (filename == NULL)
 		return (-1);
+
 	if (text_content != NULL)
 	{
-		for (lenght = 0; text_content[lenght];)
-			lenght++;
+		for (len = 0; text_content[len];)
+			len++;
 	}
-	fd = open(filename, O_WRONLY | O_APPEND);
-	w = write(fd, text_content, lenght);
-	if ( fd == -1 || w == -1)
+
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
 		return (-1);
-	close (fd);
-	return (-1);
+
+	close(o);
+
+	return (1);
 }
