eae8e5f5708fb1269c5de1d7e81297939b8eaa94
SaliheddineAbdellah <abdellah.saliheddine.101@gmail.com>
2023-09-04 09:01:29 -0700

    files io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int pfile, pw, length;
+	int pfile, wr, length = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -22,11 +22,13 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	pfile = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	pw = write(pfile, text_content, length);
+	wr = write(pfile, text_content, length);
 
-	if (pfile == -1 || pw == -1)
+	if (pfile == -1 || wr == -1)
 		return (-1);
 
 	close(pfile);
+
 	return (1);
 }
+
